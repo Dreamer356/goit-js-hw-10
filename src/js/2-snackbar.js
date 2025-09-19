19ac940bde2f253bb2c21161bbd33ccfbc5e255e
Dreamer356 <glukhovsky.v@gmail.com>
2025-09-19 21:34:50 +0300

    +

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('snackbar-form');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delay = Number(form.elements['delay'].value);
+    const status = form.elements['status'].value;
+
+    createPromise(delay, status)
+      .then(value => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled in ${value}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(value => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected in ${value}ms`,
+          position: 'topRight',
+        });
+      });
+  });
+
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        state === 'fulfilled' ? resolve(delay) : reject(delay);
+      }, delay);
+    });
+  }
+});
